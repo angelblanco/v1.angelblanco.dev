dbe86aaae226b8c5b4ec21b7f921e3c634bfb5b1
ablanco <17175344+angelblanco@users.noreply.github.com>
2020-10-18 02:25:21 +0200

    :bug: fix description meta tag test

--- cypress/support/commands/cyCheckMeta.js
+++ cypress/support/commands/cyCheckMeta.js
@@ -2,7 +2,6 @@ Cypress.Commands.add('checkMeta', () => {
     cy.title().should('not.be.empty');
 
     [
-        'description',
         'og:title',
         'og:description',
         'og:url',
@@ -24,6 +23,7 @@ Cypress.Commands.add('checkMeta', () => {
     });
 
     [
+        'description',
         'twitter:title',
         'twitter:card',
         'twitter:site',
