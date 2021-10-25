5b1b934846bd3651b237ba26962c92625900850e
angel <17175344+angelblanco@users.noreply.github.com>
2021-10-25 23:30:47 +0200

    :robot: navbar cypress test update

--- cypress/integration/navbar.spec.js
+++ cypress/integration/navbar.spec.js
@@ -10,7 +10,7 @@ describe('Navbar', () => {
 
     it('navigates to about english when clicking the nav about', () => {
       cy.get('.navbar-end a').contains('About').click();
-      cy.url().should('include', '/about/en');
+      cy.url().should('include', '/about/');
     });
 
     it('navigates to articles when clicking the nav about', () => {
