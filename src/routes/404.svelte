f24a0d6788eea5935fbc16f124a2eb51db9e4e61
angel <17175344+angelblanco@users.noreply.github.com>
2022-04-19 19:29:25 +0200

    :sunglasses: force a redirect by javascript on a 404

--- src/routes/404.svelte
+++ src/routes/404.svelte
@@ -1,5 +1,15 @@
 <script>
   import Error from './__error.svelte';
+  import { onMount } from 'svelte';
+  import trimEnd from 'lodash/trimEnd.js';
+
+  onMount(() => {
+    const href = trimEnd(window.location.href, '/');
+
+    if (href !== window.location.href) {
+      window.location.href = href;
+    }
+  });
 </script>
 
 <Error />
