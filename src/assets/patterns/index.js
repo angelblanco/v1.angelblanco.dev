c77afde20296990c95a683915b253637d6f2cc47
angel <17175344+angelblanco@users.noreply.github.com>
2022-05-08 14:06:50 +0200

    :robot: Generate images with playwtight

---
+++ src/assets/patterns/index.js
@@ -0,0 +1,7 @@
+import fallback from './fallback.svg';
+import docker from './docker.svg';
+import js from './js.svg';
+import laravel from './laravel.svg';
+import php from './php.svg';
+
+export { fallback, docker, js, laravel, php };
